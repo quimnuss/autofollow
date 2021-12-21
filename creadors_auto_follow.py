b8029573e7e8de56a680f7a8b7cbebc5a69c7fe6
quimnuss <lluna.nova@gmail.com>
2021-12-21 23:06:20 +0100

    remove unused imports, fix deprecation notices

--- creadors_auto_follow.py
+++ creadors_auto_follow.py
@@ -1,15 +1,15 @@
 import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 import selenium.webdriver.support.ui as ui
 
 with open("streamer_list.txt", "r") as f:
     content = f.read()
     creadors = content.split('\n')
 
-driver = webdriver.Firefox(executable_path=r'geckodriver.exe')
+service = Service('geckodriver.exe')
+driver = webdriver.Firefox(service=service)
 
 driver.maximize_window()
 
@@ -17,14 +17,15 @@ driver.get("http://www.twitch.tv/")
 wait = ui.WebDriverWait(driver, 60)
 
 driver.get("http://www.twitch.tv/user/login")
-driver.find_element_by_css_selector('button[data-a-target="login-button"]').click()
-time.sleep(40)
-wait.until(lambda driver: driver.find_element_by_css_selector('button[data-a-target="top-nav-get-bits-button"]'))
+driver.find_element(By.CSS_SELECTOR, 'button[data-a-target="login-button"]').click()
+time.sleep(30)
+wait.until(lambda driver: driver.find_element(By.CSS_SELECTOR,
+                                              'button[data-a-target="top-nav-get-bits-button"]'))
 
 num_creadors = len(creadors)
 for i, creador in enumerate(creadors):
     driver.get("http://www.twitch.tv/{}".format(creador))
-    follow_button = driver.find_element_by_css_selector('button[data-a-target="follow-button"]')
+    follow_button = driver.find_element(By.CSS_SELECTOR, 'button[data-a-target="follow-button"]')
     if follow_button:
         follow_button.click()
         print("Followed {} {}/{}".format(creador, i + 1, num_creadors))
