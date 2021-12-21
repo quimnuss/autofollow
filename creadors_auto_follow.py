fe128aff2274a94b86375e86953adde8256bf715
quimnuss <lluna.nova@gmail.com>
2021-12-21 23:00:20 +0100

    add streamer_list.txt to get the creadors

--- creadors_auto_follow.py
+++ creadors_auto_follow.py
@@ -5,22 +5,11 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 import selenium.webdriver.support.ui as ui
 
-# from webdriver_manager.firefox import GeckoDriverManager
+with open("streamer_list.txt", "r") as f:
+    content = f.read()
+    creadors = content.split('\n')
 
-creadors = ['creadorstv']
-
-# new_driver_path = r'C:\Users\Public\geckodriver.exe'
-# new_binary_path = r'C:\Program Files\Mozilla Firefox\firefox.exe'
-
-# ops = Options()
-# ops.binary_location = new_binary_path
-# driver = webdriver.Firefox(options=ops,executable_path=GeckoDriverManager().install())
-# driver = webdriver.Firefox(executable_path= r'C:\Program Files\Mozilla Firefox\firefox.exe')
-# this works
-# driver = webdriver.Firefox(executable_path= r'C:\Users\Public\geckodriver.exe')
-driver = webdriver.Firefox(executable_path= r'geckodriver.exe')
-# binary = FirefoxBinary('C:\Program Files\Mozilla Firefox\firefox.exe')
-# driver = webdriver.Firefox(firefox_binary=binary)
+driver = webdriver.Firefox(executable_path=r'geckodriver.exe')
 
 driver.maximize_window()
 
@@ -31,11 +20,14 @@ driver.get("http://www.twitch.tv/user/login")
 driver.find_element_by_css_selector('button[data-a-target="login-button"]').click()
 time.sleep(40)
 wait.until(lambda driver: driver.find_element_by_css_selector('button[data-a-target="top-nav-get-bits-button"]'))
-for creador in creadors:
+
+num_creadors = len(creadors)
+for i, creador in enumerate(creadors):
     driver.get("http://www.twitch.tv/{}".format(creador))
     follow_button = driver.find_element_by_css_selector('button[data-a-target="follow-button"]')
     if follow_button:
         follow_button.click()
+        print("Followed {} {}/{}".format(creador, i + 1, num_creadors))
     else:
         print("Follow button for {} not found. You might be a follower already.".format(creador))
 
